ec46d7687a2bc4a067625d4566389a865588cf3e
tommy <thmorriss@gmail.com>
2022-11-06 13:06:18 -0500

    cleaned up scripts

--- make_egg.sh
+++ make_egg.sh
@@ -1,2 +1,4 @@
-cat egg.json | jq --rawfile test install.sh '.scripts.installation.script = $test'
+cat egg.INCOMPLETE.json | jq --rawfile test install.sh '.scripts.installation.script = $test' > egg.json
+
+echo "made egg.json by putting install.sh inside egg.INCOMPLETE.json"
 
