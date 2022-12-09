a84c4e41e4bb0f8aad5d1768314b9d2cb4d5fbbb
tommy <thmorriss@gmail.com>
2022-12-08 20:39:04 -0500

    updated script

--- scripts/getmmsm.sh
+++ scripts/getmmsm.sh
@@ -3,11 +3,11 @@
 echo "running from git"
 
 
-mm_url="https://mms.alliedmods.net/mmsdrop/1.11/mmsource-1.11.0-git1148-linux.tar.gz"
-sm_url="https://www.sourcemod.net/smdrop/1.12/sourcemod-1.12.0-git6906-linux.tar.gz"
+mm_url="https://mms.alliedmods.net/mmsdrop/1.12/mmsource-1.12.0-git1164-linux.tar.gz"
+sm_url="https://sm.alliedmods.net/smdrop/1.11/sourcemod-1.11.0-git6922-linux.tar.gz"
 
-mm_dest="mm-1.11.1148.tgz"
-sm_dest="sm-1.12.6906.tgz"
+mm_dest="mm-1.12.1164.tgz"
+sm_dest="sm-1.11.6922.tgz"
 
 export destfolder="mmsm_xtracted"
 
